cdf7c0a873c27e09af694263b1d15dfcb1e90b2a
Yogi Andre Ananta <ahmadnizamfalasifai@gmail.com>
2026-09-19 15:15:22 +0700

    Update social media icons

--- thrift-katalog/lib/admin.ts
+++ thrift-katalog/lib/admin.ts
@@ -1,7 +1,12 @@
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "./firebase";
+import { db } from "@/lib/firebase";
 
 export async function isAdmin(uid: string) {
+  console.log("CHECK ADMIN UID:", uid);
+
   const snap = await getDoc(doc(db, "admins", uid));
+
+  console.log("ADMIN DOCUMENT EXISTS:", snap.exists());
+
   return snap.exists();
 }
